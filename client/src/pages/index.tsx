c3e8406e01a0e1edd25d53e8ebd79a8c438b4a18
Dawid Lachowicz <domdaw7@gmail.com>
2023-03-26 20:06:36 +0100

    Generate client code using @openapitools/openapi-generator-cli

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import ContactDetails from "@/components/Listing/ContactDetails";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -26,7 +27,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -58,6 +59,8 @@ export default function Home() {
             />
           </div>
         </div>
+
+        <ContactDetails phoneNumber={null} emailAddress={null} />
 
         <div className={styles.grid}>
           <a
@@ -119,5 +122,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
