9fd55e52a3f50761bdfe48119d97da3797bd1e8f
Duy Chinh Dinh (Tony) <95660229+tonyfromvietnam@users.noreply.github.com>
2023-03-30 01:00:20 +0100

    Update component
    Update the default height, and add an additional argument for the width of the component

--- client/src/components/PhotoGallery/index.tsx
+++ client/src/components/PhotoGallery/index.tsx
@@ -8,22 +8,26 @@ const divSettings = {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    backgroundSize: "cover"       
+    backgroundSize: "cover",
+    height: "350px"           
 }
 
 interface PhotoGalleryProps {
     photoUrls: Array<String>;
+    width: String;
 }
 
-const PhotoGallery: FC<PhotoGalleryProps> = ({ photoUrls }) => {    
-    return (        
-        <Slide transitionDuration={200}>
-        {photoUrls.map((photoUrls) => (
-            <div style={divSettings}>
-                <img src={photoUrls.toString()} width={"100%"}/>
-            </div>
-            ))}
-        </Slide>             
+const PhotoGallery: FC<PhotoGalleryProps> = ({ photoUrls, width }) => {    
+    return (
+        <div style={{width:width.toString()}}>              
+            <Slide transitionDuration={200}>
+            {photoUrls.map((photoUrls) => (        
+                <div style={divSettings}>
+                    <img src={photoUrls.toString()} width={"100%"}/>
+                </div>            
+                ))}
+            </Slide>                 
+        </div>
     );
 };
 
