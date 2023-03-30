40a346a7a4f796edf72024f1ae78e43e85fe2372
Duy Chinh Dinh (Tony) <95660229+tonyfromvietnam@users.noreply.github.com>
2023-03-30 11:52:43 +0100

    Fix the formatting of the file

--- client/src/components/PhotoGallery/index.tsx
+++ client/src/components/PhotoGallery/index.tsx
@@ -1,38 +1,40 @@
-import { FC } from 'react';
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css';
-
+import { FC } from "react";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 
 // Default settings for the slider component.
 const divSettings = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundSize: "cover"    
-}
-
-interface PhotoGalleryProps {
-    photoUrls: Array<String>;
-    width?: String;
-}
-
-const PhotoGallery: FC<PhotoGalleryProps> = ({ photoUrls, width }) => {        
-    if (typeof width === "undefined") {
-        width = "350px";
-    }
-
-    return (
-        <div style={{width:width.toString()}}>
-            <Slide transitionDuration={200}>
-            {photoUrls.map((photoUrls, index) => (        
-                <div style={divSettings} key={index}>
-                    <img src={photoUrls.toString()} width={"100%"} alt={"Photo: " + index}/>
-                </div>            
-                ))}
-            </Slide>                 
-        </div>
-    );
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundSize: "cover",
 };
 
+interface PhotoGalleryProps {
+  photoUrls: Array<String>;
+  width?: String;
+}
+
+const PhotoGallery: FC<PhotoGalleryProps> = ({ photoUrls, width }) => {
+  if (typeof width === "undefined") {
+    width = "350px";
+  }
+
+  return (
+    <div style={{ width: width.toString() }}>
+      <Slide transitionDuration={200}>
+        {photoUrls.map((photoUrls, index) => (
+          <div style={divSettings} key={index}>
+            <img
+              src={photoUrls.toString()}
+              width={"100%"}
+              alt={"Photo: " + index}
+            />
+          </div>
+        ))}
+      </Slide>
+    </div>
+  );
+};
 
 export default PhotoGallery;
