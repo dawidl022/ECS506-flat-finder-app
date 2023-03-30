8b5de33ba9720aed6c1da72eb255944ea51807a2
Dawid Lachowicz <domdaw7@gmail.com>
2023-03-30 22:16:49 +0100

    Add custom CamelCase JSON encoder to conform with API spec

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from app.util.schema import CamelCaseEncoder
 from config import Config
 
 
 def create_app(config_class=Config) -> Flask:
     app = Flask(__name__)
     app.config.from_object(config_class)
+    app.json_encoder = CamelCaseEncoder
 
     # Initialize Flask extensions here
 
--- backend/app/util/schema.py
+++ backend/app/util/schema.py
@@ -1,5 +1,6 @@
 
 import abc
+import json
 from marshmallow import Schema
 
 
@@ -13,9 +14,24 @@ def camel_case(s):
 
 
 class CamelCaseSchema(Schema):
-    """Schema that uses camel-case for its external representation
+    """
+    Schema that uses camel-case for its external representation
     and snake-case for its internal representation.
     """
 
     def on_bind_field(self, field_name, field_obj):
         field_obj.data_key = camel_case(field_obj.data_key or field_name)
+
+
+class CamelCaseEncoder(json.JSONEncoder):
+    """
+    Custom encoder that converts all snake_case object property names into
+    camelCase to conform with the naming convention used in our API spec
+    """
+
+    def encode(self, obj: object):
+        obj = {camel_case(k): v for k, v in obj.__dict__.items()}
+        for key, value in obj.items():
+            if hasattr(value, "__dict__"):
+                obj[key] = json.loads(self.encode(value))
+        return super().encode(obj)
