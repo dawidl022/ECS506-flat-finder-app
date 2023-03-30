077bf10d6b6d611dd5a53fdd01b6488785581c46
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2023-03-30 22:02:34 +0100

    feature: working tabs. when selected changing query params

--- client/src/components/Tabs/index.tsx
+++ client/src/components/Tabs/index.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { FC } from 'react';
 
-const Tabs = () => {
-  return <div>Tabs</div>;
+import styles from './Tabs.module.scss';
+
+interface TabProps {
+  isActive: boolean;
+  title: string;
+  onClick: (v: string) => void;
+}
+
+const TabItem: FC<TabProps> = ({ title, onClick, isActive }) => {
+  return (
+    <div
+      onClick={() => onClick(title.toLowerCase())}
+      className={isActive ? styles.activeTabCon : styles.tabCon}>
+      {title}
+    </div>
+  );
+};
+
+interface TabsComponentProps {
+  tabs: string[];
+}
+
+const Tabs: FC<TabsComponentProps> = ({ tabs }) => {
+  const router = useRouter();
+  const { push, query, pathname } = router;
+
+  const test = (v: string) => {
+    if (v === 'all') return push(pathname, undefined, { shallow: true });
+    push({ query: { ...query, listing: v } }, undefined, { shallow: true });
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      {tabs?.map((tab: string, index: number) => (
+        <TabItem
+          isActive={
+            tab.toLowerCase() === query.listing || (tab === 'All' && query.listing === undefined)
+          }
+          onClick={(v) => test(v)}
+          title={tab}
+          key={index}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Tabs;
