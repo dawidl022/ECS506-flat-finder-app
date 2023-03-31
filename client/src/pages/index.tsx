4068f6bbbfd3e947ff1d3ff9c777cfd379de9420
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2023-03-31 21:38:19 +0100

    feature: user page and profile card component

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -15,12 +15,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <main>
         <div className="container">
-        <h2>Hello</h2>
+          <h2>Hello</h2>
         </div>
-        </main>
+      </main>
     </>
   );
 }
