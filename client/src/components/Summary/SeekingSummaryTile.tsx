58d9fa0598aa0b4034e5675b7f0c610915db7c6f
ia404 <imran.a10outlook.com>
2023-04-02 20:16:06 +0100

    changed import and link href

--- client/src/components/Summary/SeekingSummaryTile.tsx
+++ client/src/components/Summary/SeekingSummaryTile.tsx
@@ -1,22 +1,17 @@
 import { FC } from "react";
 import Link from "next/link";
-import { AccommodationSummary } from "@/generated/models/AccommodationSummary";
+import { SeekingSummary } from "@/generated/models/SeekingSummary";
 
 interface seekingSummaryProps {
-  accomodation: AccommodationSummary;
+  seekingAccomodation: SeekingSummary;
 }
 
-const SeekingSummaryTile: FC<seekingSummaryProps> = ({ accomodation }) => {
+const SeekingSummaryTile: FC<seekingSummaryProps> = ({ seekingAccomodation }) => {
   return (
     <div>
-      <Link href={`/listings/accommodation/${accomodation.id}`}>
-        <p> {accomodation.title} </p>
-        <p> {`£${accomodation.price}`} </p>
-        <p> {accomodation.shortDescription} </p>
-        <p> {accomodation.accommodationType} </p>
-        <p> {accomodation.numberOfRooms} </p>
-        <p> {accomodation.postCode} </p>
-        <p> {accomodation.source} </p>
+      <Link href={`/listings/seeking/${seekingAccomodation.id}`}>
+        <p> {seekingAccomodation.title} </p>
+        <p> {seekingAccomodation.shortDescription} </p>
       </Link>
     </div>
   );
