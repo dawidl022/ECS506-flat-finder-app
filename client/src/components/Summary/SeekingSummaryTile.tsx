fed71bc918392038873d4e58867bedea2672876c
ia404 <imran.a10outlook.com>
2023-04-02 19:26:06 +0100

    reformat file

--- client/src/components/Summary/SeekingSummaryTile.tsx
+++ client/src/components/Summary/SeekingSummaryTile.tsx
@@ -1,25 +1,25 @@
-import { FC } from 'react';
-import Link from 'next/link';
-import { AccommodationSummary } from '@/generated/models/AccommodationSummary';
+import { FC } from "react";
+import Link from "next/link";
+import { AccommodationSummary } from "@/generated/models/AccommodationSummary";
 
 interface seekingSummaryProps {
-    accomodation: AccommodationSummary;
+  accomodation: AccommodationSummary;
 }
 
 const SeekingSummaryTile: FC<seekingSummaryProps> = ({ accomodation }) => {
-    return (
-        <div>
-            <Link href={ `/listings/accommodation/${accomodation.id}` }>                
-                <p> { accomodation.title } </p>
-                <p> { `£${accomodation.price}` } </p>
-                <p> { accomodation.shortDescription } </p>
-                <p> { accomodation.accommodationType } </p>
-                <p> { accomodation.numberOfRooms } </p>
-                <p> { accomodation.postCode }  </p>
-                <p> { accomodation.source } </p>
-            </Link>
-        </div>
-    );
+  return (
+    <div>
+      <Link href={`/listings/accommodation/${accomodation.id}`}>
+        <p> {accomodation.title} </p>
+        <p> {`£${accomodation.price}`} </p>
+        <p> {accomodation.shortDescription} </p>
+        <p> {accomodation.accommodationType} </p>
+        <p> {accomodation.numberOfRooms} </p>
+        <p> {accomodation.postCode} </p>
+        <p> {accomodation.source} </p>
+      </Link>
+    </div>
+  );
 };
 
 export default SeekingSummaryTile;
