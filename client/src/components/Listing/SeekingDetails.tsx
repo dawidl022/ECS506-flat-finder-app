697989c6f1f1a2280825b6343143189b16430f6a
ia404 <imran.a10outlook.com>
2023-04-05 14:28:58 +0100

    reformat code

--- client/src/components/Listing/SeekingDetails.tsx
+++ client/src/components/Listing/SeekingDetails.tsx
@@ -7,23 +7,24 @@ interface SeekingDetailsProps {
 }
 
 const SeekingDetails: FC<SeekingDetailsProps> = ({ seeking }) => {
-  return ( <>
-    <div>
-      <h1>{seeking.title}</h1>
-      <p>{seeking.description}</p>
-    </div>
+  return (
+    <>
+      <div>
+        <h1>{seeking.title}</h1>
+        <p>{seeking.description}</p>
+      </div>
 
-    <div>
-      <h3>Contact Details</h3>
-      <ContactDetails
-        phoneNumber={seeking.contactInfo?.phoneNumber ?? null}
-        emailAddress={seeking.contactInfo?.email ?? null}
-      />
-  </div>
-  
-  <div>{/* TODO: Author details */}</div>
-    
-  </>);
+      <div>
+        <h3>Contact Details</h3>
+        <ContactDetails
+          phoneNumber={seeking.contactInfo?.phoneNumber ?? null}
+          emailAddress={seeking.contactInfo?.email ?? null}
+        />
+      </div>
+
+      <div>{/* TODO: Author details */}</div>
+    </>
+  );
 };
 
 export default SeekingDetails;
