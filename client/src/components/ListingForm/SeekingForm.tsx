7a4fafa0822b12882721a8caaaef962848d95591
ia404 <imran.a10outlook.com>
2023-04-07 18:10:43 +0100

    added alerts for file input

--- client/src/components/ListingForm/SeekingForm.tsx
+++ client/src/components/ListingForm/SeekingForm.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, FormEvent } from "react";
+import { FC, useState, FormEvent, ChangeEvent } from "react";
 import { useRouter } from "next/router";
 import { Configuration, DefaultApi } from "@/generated";
 
@@ -8,6 +8,28 @@ const SeekingForm: FC = ({}) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [location, setLocation] = useState("");
+
+  function handleFileInput(e: ChangeEvent<HTMLInputElement>) {
+    const files = e.target.files;
+    //check if there are more than 15 files selected
+    if (files && files.length > 0) {
+      if (files.length > 15) {
+        alert("You can upload up to 15 files");
+        e.target.value = '';
+        return;
+      }
+
+      //check if the file size is greater than 5MB
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        if (file.size > 5242880) {
+          alert(`The maximum file size is 5MB`);
+          e.target.value = ''; 
+          return;
+        }
+      }
+    } 
+  }
 
   const preview = () => {
     router.push({
@@ -85,8 +107,8 @@ const SeekingForm: FC = ({}) => {
           <input
             type="file"
             id="photos"
-            placeholder="Import Photos OPTIONAL"
-            accept="image/png, image/jpeg"
+            onChange={handleFileInput}
+            accept="image/*"
             multiple
           />
           <br />
