0d2fee69af5cb2f83339f60f7932d22e5b357187
ia404 <imran.a10outlook.com>
2023-04-09 00:24:11 +0100

    add forms to editlisting.tsx

--- client/src/components/EditListing/EditListing.tsx
+++ client/src/components/EditListing/EditListing.tsx
@@ -1,14 +1,17 @@
 import {FC} from 'react';
-
+import SeekingForm from './SeekingForm';
+import AccommodationForm from './AccommodationForm';
 interface EditListingProps {
-    listingType: "seeking" | "accommodation";
+    listingType: boolean;
     listingId: string;
 }
 
 const EditListing: FC<EditListingProps> = (listingType, listingId) => {
     return (
         <div>
+            {listingType && <SeekingForm listingId={listingId} />}
+            {!listingType && <AccommodationForm listingId={listingId} />}
         </div>
-    )
+    );
 }
 export default EditListing;
