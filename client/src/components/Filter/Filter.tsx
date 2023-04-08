6fbe3562964aa1da5bbf29805221c0440841d665
ia404 <imran.a10outlook.com>
2023-04-08 11:41:52 +0100

    user must checkbox atleast one

--- client/src/components/Filter/Filter.tsx
+++ client/src/components/Filter/Filter.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, FormEvent, useState } from "react";
 import { Configuration, DefaultApi } from "@/generated";
 
 interface FilterProps {
@@ -9,42 +9,65 @@ interface FilterProps {
 
 const Filter: FC<FilterProps> = ({ sources, maxPrice, handleApply }) => {
   const [chooseSources, setSource] = useState(sources);
+  //used to ensure that there is always 1 source selected
+  const [selectOneSource, setSelected] = useState(false);
   //set the max price to 0 if the prop is null
   const [price, setPrice] = useState(maxPrice || 0);
+
+  const checkIfOneSourceIsSelected = () => {
+    const selected = Object.values(chooseSources).some(value => value);
+    setSelected(selected);
+    return selected;
+  };
+  
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!checkIfOneSourceIsSelected()) {
+        alert("Please select at least one source.");
+        //reset form
+        setSource(sources);
+    }
+    // form submission
+  };
+  
+
   return (
     <div>
-      <input
-        type="number"
-        name="maxPrice"
-        id="maxPrice"
-        value={price}
-        min={0}
-        onChange={e => setPrice(parseInt(e.target.value))}
-      />
-      {
-        /* creating a checkbox per source by mapping the dictionary */ sources &&
-          Object.keys(sources).map((source, index) => {
-            return (
-              <div key={index}>
-                <label htmlFor={source}>{source} </label>
-                <input
-                  type="checkbox"
-                  name={source}
-                  id={source}
-                  checked={chooseSources[source]}
-                  onChange={() =>
-                    setSource({
-                      ...chooseSources,
-                      [source]: !chooseSources[source],
-                    })
-                  }
-                />
-              </div>
-            );
-          })
-      }
+        <form onSubmit={handleSubmit}>
+        <input
+            type="number"
+            name="maxPrice"
+            id="maxPrice"
+            value={price}
+            min={0}
+            onChange={e => setPrice(parseInt(e.target.value))}
+        />
+        {
+            /* creating a checkbox per source by mapping the dictionary */ sources &&
+            Object.keys(sources).map((source, index) => {
+                return (
+                <div key={index}>
+                    <label htmlFor={source}>{source} </label>
+                    <input
+                    type="checkbox"
+                    name={source}
+                    id={source}
+                    checked={chooseSources[source]}
+                    onChange={() =>
+                        setSource({
+                          ...chooseSources,
+                          [source]: !chooseSources[source],
+                        })
+                      }
+                      
+                    />
+                </div>
+                );
+            })
+        }
 
-      <button onClick={() => handleApply(chooseSources, price)}>Apply</button>
+            <button>Apply</button>
+        </form>
     </div>
   );
 };
