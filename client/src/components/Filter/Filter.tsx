50d45055ad9676d7689957eea92fcd9387628907
ia404 <imran.a10outlook.com>
2023-04-08 11:48:05 +0100

    added fieldset for future styling

--- client/src/components/Filter/Filter.tsx
+++ client/src/components/Filter/Filter.tsx
@@ -16,56 +16,57 @@ const Filter: FC<FilterProps> = ({ sources, maxPrice, handleApply }) => {
     const selected = Object.values(chooseSources).some(value => value);
     return selected;
   };
-  
+
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     if (!checkIfOneSourceIsSelected()) {
-        alert("Please select at least one source.");
-        //reset form
-        setSource(sources);
+      alert("Please select at least one source.");
+      //reset form
+      setSource(sources);
     } else {
-        // form submission
-        handleApply(chooseSources, price);
+      // form submission
+      handleApply(chooseSources, price);
     }
   };
 
   return (
     <div>
-        <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
-            type="number"
-            name="maxPrice"
-            id="maxPrice"
-            value={price}
-            min={0}
-            onChange={e => setPrice(parseInt(e.target.value))}
+          type="number"
+          name="maxPrice"
+          id="maxPrice"
+          value={price}
+          min={0}
+          onChange={e => setPrice(parseInt(e.target.value))}
         />
-        {
+
+        <fieldset>
+          {
             /* creating a checkbox per source by mapping the dictionary */ sources &&
-            Object.keys(sources).map((source, index) => {
+              Object.keys(sources).map((source, index) => {
                 return (
-                <div key={index}>
+                  <div key={index}>
                     <label htmlFor={source}>{source} </label>
                     <input
-                    type="checkbox"
-                    name={source}
-                    id={source}
-                    checked={chooseSources[source]}
-                    onChange={() =>
+                      type="checkbox"
+                      name={source}
+                      id={source}
+                      checked={chooseSources[source]}
+                      onChange={() =>
                         setSource({
                           ...chooseSources,
                           [source]: !chooseSources[source],
                         })
                       }
-                      
                     />
-                </div>
+                  </div>
                 );
-            })
-        }
-
-            <button>Apply</button>
-        </form>
+              })
+          }
+        </fieldset>
+        <button>Apply</button>
+      </form>
     </div>
   );
 };
