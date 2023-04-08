ef8e791cd862785787f063d95b7a6730bb48b7c0
Đinh <cb20079@qmul.ac.uk>
2023-04-08 18:54:05 +0100

    Build the Infinite Listings component

---
+++ client/src/components/InfiniteListings/index.tsx
@@ -0,0 +1,64 @@
+import { FC, useState, useEffect } from "react";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { ApiV1ListingsAccommodationGetSortByEnum, DefaultApi } from "@/generated/apis/DefaultApi";
+import AccommodationSummaryTile from "@/components/Summary/AccomodationSummaryTile";
+import { AccommodationSearchResultsInner } from "@/generated/models/AccommodationSearchResultsInner";
+
+interface AccommodationDetailsProps {
+    location: string;
+    radius: number;
+    maxPrice?: number;
+    sources?: Array<string>;
+    sortBy?: ApiV1ListingsAccommodationGetSortByEnum;
+    page?: number;
+    size?: number;
+}
+
+const InfiniteListings: FC<AccommodationDetailsProps> = ({ location, radius, maxPrice, sources, sortBy, page=0, size=6 }) => {
+  const [data, setData] = useState(Array<AccommodationSearchResultsInner>);
+  const [error, setError] = useState(false);
+
+  const getMoreAccommodation = async () => {    
+    new DefaultApi()
+      .apiV1ListingsAccommodationGet({
+        location: location,
+        radius: radius,
+        maxPrice: maxPrice,
+        sources: sources,
+        sortBy:sortBy,
+        page: page,
+        size: size
+      })
+    .then(res => {setData(data.concat(res))})
+    .catch(() => setError(true));
+    page = page + 1;
+  };
+
+  return (    
+    <div>
+      {error ? (
+        <p>Error fetching data</p>
+      ) : (    
+        <InfiniteScroll
+            dataLength={data.length}
+            next={getMoreAccommodation}
+            hasMore={!error}
+            loader={<h4>Loading...</h4>}
+            endMessage={
+                <p style={{ textAlign: 'center' }}>
+                    <b>End of results</b>
+                </p>
+            }
+        >
+            {data.map((data, index) => (
+                <div key={index}>
+                    <AccommodationSummaryTile accommodation={data.accommodation}/>
+                </div>
+            ))}
+        </InfiniteScroll>        
+      )}
+    </div>
+  );
+};
+
+export default InfiniteListings;
