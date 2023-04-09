d6de16f5025ee321677bb85343bb009503260002
ia404 <imran.a10outlook.com>
2023-04-09 16:58:25 +0100

    changed from boolean

--- client/src/components/EditListing/EditListing.tsx
+++ client/src/components/EditListing/EditListing.tsx
@@ -1,16 +1,17 @@
 import { FC } from "react";
 import SeekingForm from "./SeekingForm";
 import AccommodationForm from "./AccommodationForm";
+
 interface EditListingProps {
-  listingType: boolean; //true means seeking, false means accommodation
+  listingType: "seeking" | "accommodation";
   listingId: string;
 }
 
 const EditListing: FC<EditListingProps> = (listingType, listingId) => {
   return (
     <div>
-      {listingType && <SeekingForm listingId={listingId} />}
-      {!listingType && <AccommodationForm listingId={listingId} />}
+      {listingType.toString() === "seeking" && <SeekingForm listingId={listingId} />}
+      {listingType.toString() === "accommodation" && <AccommodationForm listingId={listingId} />}
     </div>
   );
 };
