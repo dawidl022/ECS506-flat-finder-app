f2df6ad665c0c9a814c36af887453722374e39bd
ia404 <imran.a10@outlook.com>
2023-04-13 01:39:45 +0100

    add placeholder

--- client/src/pages/SeekingPreviewPage.tsx
+++ client/src/pages/SeekingPreviewPage.tsx
@@ -20,7 +20,6 @@ const SeekingPreviewPage: FC = ({}) => {
         },
       },
     },
-    //link photourl to placeholder image in public folder
     photoUrls: ["placeholder.webp"],
     contactInfo: {
       phoneNumber: "Dummy Phone Number",
